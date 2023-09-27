5ff53b3ab05e5deceb6a91b46f917eabaec804ca
facamesi52 <facamesi52@gmail.com>
2023-09-26 23:13:46 -0500

    funcionado todo empezar migracion a pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,46 @@
 "use client"
 import React from 'react'
 import { useState } from 'react'
+import { createMessage } from '../../service/Qr.controller'
 import './contact.css'
-const Contact = () => {
-  const [formData, setFormData] = useState({
-    nameMessage: 'anonymous',
+type FormData = {
+  nameMessage: string;
+  message: string;
+}
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    nameMessage: '',
     message:'',
   })
   const { nameMessage, message } = formData;
-  const handleInputChange = (event:any) => {
+  const queryParams = new URLSearchParams(location.search);
+  const id = queryParams.get('userId');
+  console.log('este es el id que encuentro en la url', id)
+  const handleInputChange = (event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setFormData({...formData, [name]: value});
   };
-  const handleSubmit = (event:any) =>{
+  const handleSubmit = async (event:React.FormEvent) =>{
     event.preventDefault();
-  }
+    const newMessage = {
+      ...formData,
+    }
+    try {
+      if (id) {
+        // Pasa el userId a la función createMessage
+        let response = await createMessage(id, newMessage);
+        setFormData({
+          nameMessage: '',
+          message: '',
+        });
+        console.log('Mensaje creado con éxito:', response);
+      } else {
+        console.error('No se encontró userId en la consulta.');
+      }
+    } catch (error) {
+      console.error('Error al crear el mensaje:', error);
+    }
+  };
   return (
     <div>
       <div className='imageIconContact'>
@@ -24,26 +50,28 @@ const Contact = () => {
       <div className="mx-auto max-w-2xl p-4">
       <h2 className="text-2xl font-semibold mb-4">Provide Your Information</h2>
       <div className="mb-4">
-        <label htmlFor="nombre" className="block text-sm font-medium text-gray-800">
-          Name <span className="text-gray-400">(optional)</span>
+        <br />
+        <label htmlFor="nameMessage" className="block text-sm font-medium text-gray-800">
+          Contact <span className="text-gray-400">(optional)</span>
         </label>
         <input
           type="text"
-          id="nombre"
-          name="nombre"
+          id="nameMessage"
+          name="nameMessage"
           value={nameMessage}
           onChange={handleInputChange}
-          placeholder="Write your name"
+          placeholder="Write a way for me to contact you"
           className="w-full px-4 py-2 rounded-lg border border-gray-400 focus:outline-none focus:ring focus:border-blue-300"
         />
       </div>
+      <br />
       <div className="mb-4">
-        <label htmlFor="bio" className="block text-sm font-medium text-gray-800">
+        <label htmlFor="message" className="block text-sm font-medium text-gray-800">
         Message
         </label>
         <textarea
-          id="bio"
-          name="bio"
+          id="message"
+          name="message"
           value={message}
           onChange={handleInputChange}
           rows={4}
@@ -51,6 +79,7 @@ const Contact = () => {
           className="w-full px-4 py-2 rounded-lg border border-gray-400 focus:outline-none focus:ring focus:border-blue-300"
         />
       </div>
+      <br />
       <div className="flex justify-center">
         <button
           type="submit"
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,33 +1,109 @@
 'use client'
+import { useEffect, useState } from "react";
 import { QRCode } from "react-qrcode";
 import './dashboard.css'
 import { UserButton, useUser } from "@clerk/nextjs";
+import { createUserQr, getUserById } from '../../service/Qr.controller';
+
+type Message = {
+  messageText: string;
+  emailMessage: string;
+};
 
 const DashboardPage = () => {
+  const { user } = useUser();
+  const id = user?.id;
+  const userName = user?.username;
+  const userEmail = user?.primaryEmailAddress?.emailAddress;
 
-    const { user } = useUser();
-    const id = user?.id;
-    const userName = user?.username
-    const userEmail = user?.primaryEmailAddress?.emailAddress
-    console.log(id, userName, userEmail);
+  // Estado para almacenar los mensajes
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const checkAndCreateUser = async () => {
+      // Verificar si el usuario ya existe
+      const existingUser = await getUserById(id);
+
+      if (!existingUser) {
+        // Si el usuario no existe, crearlo
+        const userData = {
+          id,
+          email: userEmail,
+          userName,
+        };
+
+        try {
+          // Enviar la solicitud para crear el usuario
+          const result = await createUserQr(userData);
+
+          if (result.success) {
+            console.log('Usuario creado con éxito:', result.data);
+            // Aquí puedes realizar cualquier acción adicional, como mostrar un mensaje de éxito
+          } else {
+            console.error('Error al crear el usuario:', result.error_message);
+            // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
+          }
+        } catch (error) {
+          console.error('Error al crear el usuario:', error);
+          // Manejar errores de red u otros errores inesperados
+        }
+      } else {
+        console.log('El usuario ya existe:', existingUser.data?.data?.messages);
+
+        // Asegurarse de que existingUser.data?.data?.messages sea un arreglo
+        if (Array.isArray(existingUser.data?.data?.messages)) {
+          // Aquí puedes guardar los mensajes en el estado
+          setMessages(existingUser.data.data.messages);
+        }
+        // Aquí puedes realizar cualquier acción adicional si el usuario ya existe
+      }
+    };
+
+    // Llamar a la función de verificación y creación de usuario cuando se monte el componente
+    checkAndCreateUser();
+  }, [id, userName, userEmail]);
+
   return (
     <div className="contenedorDash">
       <div className="encabezadoDash">
         <div><h3>{`Welcome ${userName}`}</h3></div>
-      <UserButton/>
+        <UserButton />
       </div>
       <br />
-        <h2>Emergency QR</h2>
-        <QRCode
+      <h2>Emergency QR</h2>
+      <QRCode
         value={`/contact/${id}`}
         size={400}
         scale={10}
       />
-      <h4> You can print this QR so people can contact you. This QR is unique </h4>
+      <h4>You can print this QR so people can contact you. This QR is unique</h4>
       <br />
-
+  
+      {/* Aplica estilos específicos para la tabla */}
+      <div className="messages">
+        {messages.length > 0 ? (
+          <table className="tabla-mensajes">
+            <thead>
+              <tr>
+                <th>Message</th>
+                <th>Sent by</th>
+              </tr>
+            </thead>
+            <tbody>
+              {messages.map((message, index) => (
+                <tr key={index}>
+                  <td>{message.messageText}</td>
+                  <td>{message.emailMessage}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No hay mensajes.</p>
+        )}
+      </div>
     </div>
   )
 }
 
-export default DashboardPage
+export default DashboardPage;
